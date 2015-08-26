74edefc7e05f36ec7134e84d2f7d8112456f887a
Vit Stanislav <slaweet@seznam.cz>
2015-08-26 19:57:28 +0200

    Set page headline on preloading

--- anatomy/views.py
+++ anatomy/views.py
@@ -29,7 +29,7 @@ def home(request, hack=None):
         email = request.user.email
     c = {
         'title': _(u'Anatom.cz') + ' - ' + _(u'inteligentní aplikace na procvičování anatomie'),
-        'map': get_map_from_url(hack),
+        'headline': get_headline_from_url(hack),
         'is_production': settings.ON_PRODUCTION,
         'css_files': CSS_FILES,
         'js_files': JS_FILES,
@@ -46,17 +46,17 @@ def home(request, hack=None):
     return render_to_response('home.html', c)
 
 
-def get_map_from_url(hack):
-    map_string = ""
+def get_headline_from_url(hack):
+    headline = ""
     if hack:
         url = hack.split('/')
         if url[0] == u'view' or url[0] == u'practice':
-            map_code = url[1]
             try:
-                map_category = Category.objects.get(lang=get_language(), identifier=map_code)
-                map_string = map_category.name
-                if len(url) >= 3 and url[2] != '':
-                    map_string = map_string  # + ' - ' + resolve_map_type(url[2])
+                category = Category.objects.get(
+                    lang=get_language(), identifier=url[1])
+                headline = category.name
             except Category.DoesNotExist:
                 pass
-    return map_string
+        elif url[0] == u'overview':
+            headline = _(u'Předhled znalostí')
+    return headline
