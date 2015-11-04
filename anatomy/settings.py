0caec1a8b1af27da4e1b16ca9803720feee0851e
Jan Papousek <jan.papousek@gmail.com>
2015-11-04 13:17:17 +0100

    send also warnings by e-mail

--- anatomy/settings.py
+++ anatomy/settings.py
@@ -204,7 +204,7 @@ LOGGING = {
             'formatter': 'simple'
         },
         'mail_admins': {
-            'level': 'ERROR',
+            'level': 'WARNING',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
