fd5a1294b33842d2ef1c2fd2440501a3123f51a3
Jan Papousek <jan.papousek@gmail.com>
2016-08-10 16:09:07 +0200

    use locmem cache instead of dummy one

--- anatomy/settings.py
+++ anatomy/settings.py
@@ -274,7 +274,7 @@ elif ON_STAGING:
 else:
     CACHES = {
         'default': {
-            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         }
     }
 
