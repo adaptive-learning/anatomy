640888583a572ca9de583162537c919554a16b2b
Vít Stanislav <slaweet@gmail.com>
2016-10-13 14:53:07 +0200

    Load subscription plans on deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ python $DIR/manage.py migrate --noinput
 python $DIR/manage.py compilemessages
 python $DIR/manage.py load_user_questions $DIR/data/user-questions.json
 python $DIR/manage.py load_configab_experiments $DIR/ab_experiments.json
+python $DIR/manage.py load_subscription_plans $DIR/plans.json
 
 cd $DIR/anatomy
 npm install --python=/usr/bin/python2.7
