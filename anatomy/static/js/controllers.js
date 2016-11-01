e9c49615bcb242eb9bbe8db863614099d96d51dc
Vít Stanislav <slaweet@gmail.com>
2016-11-01 08:25:58 +0100

    Handle discount code errors

--- anatomy/static/js/controllers.js
+++ anatomy/static/js/controllers.js
@@ -565,17 +565,39 @@ angular.module('proso.anatomy.controllers', [])
     };
 }])
 
-.controller('PremiumController', ['$scope', 'userService', 'subscriptionService', 'smoothScroll', '$routeParams',
-    function($scope, userService, subscriptionService, smoothScroll, $routeParams){
+.controller('PremiumController', ['$scope', 'userService', 'subscriptionService', 'smoothScroll', '$routeParams', 'gettextCatalog',
+    function($scope, userService, subscriptionService, smoothScroll, $routeParams, gettextCatalog){
+
+  var errorMessages = {
+    'discount_code_limit_exceeded' : gettextCatalog.getString(
+      "Zadaný slevový kód byl již použit maximálním počtem uživatelů."),
+    'discount_code_already_used' : gettextCatalog.getString(
+      "Zadaný slevový kód nemůžež použít podruhé."),
+  };
+
   subscriptionService.getPlans($routeParams.discount_code).success(function(data) {
     $scope.plans = data.data;
   }).error(function(data, status) {
     if ($routeParams.discount_code && status == 404) {
-      $scope.invalidCode = true;
+      $scope.error = gettextCatalog.getString(
+        "Byl zadán neplatný slevový kód '{{discountCode}}'.");
+    } else if ($routeParams.discount_code && status == 400) {
+      $scope.error = errorMessages[data.error_type];
     } else {
       $scope.error = true;
     }
-  });
+  }).finally(scrollToPlans);
+
+
+  function scrollToPlans() {
+    if ($routeParams.discount_code) {
+      var elem = document.getElementById("plans");
+      smoothScroll(elem, {
+        offset: 10,
+        duration: 200,
+      });
+    }
+  }
 
   $scope.userService = userService;
   $scope.discountCode = $routeParams.discount_code;
