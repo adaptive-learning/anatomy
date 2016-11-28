94e0389032f2855297cc3ab627c42456b55f24b1
Vít Stanislav <slaweet@gmail.com>
2016-11-28 15:12:44 +0100

    Generate base64 encoded tiny templates

--- anatomy/management/commands/generate_thumbnails.py
+++ anatomy/management/commands/generate_thumbnails.py
@@ -4,15 +4,40 @@ from django.conf import settings
 import os
 from wand.image import Image, Color
 from clint.textui import progress
+import base64
+import json
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        self.tiny_thumbnails = {}
+
         contexts = Context.objects.filter(lang='cs')
         print("Generating thumbnails")
         for c in progress.bar(contexts, every=max(1, len(contexts) // 100)):
             self.generate_thumbnail(c)
+
+        path = "anatomy/static/img/"
+        for image in os.listdir(path):
+            if image.endswith('.png'):
+                self.add_to_tiny_thumbnails(path + image, image, '/static/img/')
+
+        thumbnails_json_file = 'anatomy/static/dist/thumbnails.json'
+        with open(thumbnails_json_file, 'w') as outfile:
+            json.dump(self.tiny_thumbnails, outfile)
+
+    def add_to_tiny_thumbnails(self, file_path, file_name, online_path):
+        with Image(filename=file_path) as img:
+            online_path = online_path + file_name
+            img.transform(resize='16x16')
+            tmp_path = file_path + '.tpm'
+            img.save(filename=tmp_path)
+            img.save(filename=tmp_path)
+            with open(tmp_path, "rb") as image_file:
+                encoded_image = base64.b64encode(image_file.read())
+                self.tiny_thumbnails[online_path] = encoded_image.decode("utf-8")
+            os.remove(tmp_path)
 
     def generate_thumbnail(self, context):
         file_name = context.identifier + '.png'
@@ -32,3 +57,4 @@ class Command(BaseCommand):
                 img.alpha = True
                 img.modulate(saturation=0.5)
                 img.save(filename=new_path)
+                self.add_to_tiny_thumbnails(new_path, file_name, '/media/tile-thumbs/')
